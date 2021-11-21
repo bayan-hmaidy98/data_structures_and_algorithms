750419be019848859ce5c1f3c870e60568f2464a
bayan-hmaidy98 <bayanhdy98@gmail.com>
2021-11-22 01:03:45 +0200

    finish code challenge

---
+++ java/HashTables/app/src/main/java/HashTables/HashTable.java
@@ -0,0 +1,99 @@
+package HashTables;
+
+import java.util.ArrayList;
+
+public class HashTable <K, V>{
+
+  public int size;
+  public int buckets;
+
+  public ArrayList<HashNode> table;
+
+  public HashTable(){
+
+    table = new ArrayList<>();
+    size = 0;
+    buckets = 10;
+
+    for (int i = 0; i < buckets; i++) {
+      table.add(null);
+    }
+  }
+
+  public int hash(K key) {
+    return (Math.abs(key.hashCode())) % buckets;
+  }
+
+
+  public V get(K key){
+    int keyIndex = hash(key);
+    HashNode<K,V> head = table.get(keyIndex);
+
+    while (head != null){
+      if (head.key.equals(key)) return head.value;
+      head = head.next;
+    }
+    return null;
+  }
+
+  public void add(K key, V value){
+
+    int keyIndex = hash(key);
+    HashNode head = table.get(keyIndex);
+
+    while (head != null){
+      // in case we have found the same key already exits in the linked list, there's no need to add it
+      if (head.key.equals(key)) break;
+      head = head.next;
+    }
+    size++;
+    // after moving the head pointer, we need to rest it again
+    head = table.get(keyIndex);
+
+    // now, we need to create a new node and assert it to the linked list.
+
+    HashNode newNode = new HashNode(key, value);
+    // in linked list we assert the node to the beginning of the linked list.
+    newNode.next = head;
+    // we add to the array in the index we calculated the new node we have created.
+    table.set(keyIndex, newNode);
+
+    // let's check load factor, we need to check the load factor of the array, in other word if the array is almost full or not to double the size if needed
+//    if (size*1 >= 0.7* buckets){
+//      ArrayList tempArr = table;
+//      // reinitialize the array again after saving the values, we need to do this when giving the new size of the array below
+//      buckets = buckets * 2;
+//      size = 0;
+//
+//      // now, we need to increase the size of the array
+//      for (int i = 0; i < buckets; i++){
+//        table.add(null);
+//      }
+//
+//      // copy the values in temp
+//      for (HashNode head: tempArr){
+//
+//      }
+//      for (HashNode headNode : tempArr) {
+//        while (headNode != null) {
+//          add(headNode.key, headNode.value);
+//          headNode = headNode.next;
+//        }
+//    }
+
+  }
+
+  public boolean contains(K key){
+
+    // this function is the same as get since we need to check the whole linked list to figure if the key exists or not
+    int keyIndex = hash(key);
+    HashNode<K,V> head = table.get(keyIndex);
+
+    while (head != null){
+      if (head.key.equals(key)) return true;
+      head = head.next;
+    }
+    return false;
+  }
+
+}
