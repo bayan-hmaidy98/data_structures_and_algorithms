1bb029fc2e6163db17669385c4bcbbe486dcec64
bayan-hmaidy98 <bayanhdy98@gmail.com>
2021-11-23 01:59:42 +0200

    not finished yet

--- java/HashTables/app/src/test/java/HashTables/AppTest.java
+++ java/HashTables/app/src/test/java/HashTables/AppTest.java
@@ -48,21 +48,21 @@ class AppTest {
     assertEquals("summer", test.repeatedWord(input3));
   }
 
-  @Test void testTreeComparison(){
-    TreeSet tree1 = new TreeSet();
-    TreeSet tree2 = new TreeSet();
-
-    tree1.add(1);
-    tree1.add(2);
-    tree1.add(3);
-    tree1.add(4);
-
-    tree2.add(1);
-    tree2.add(4);
-    tree2.add(9);
-    tree2.add(16);
-
-    TreeIntersection treeIntersection = new TreeIntersection();
-    System.out.println(treeIntersection.treeComparing(tree1,tree2).toString());
-  }
+//  @Test void testTreeComparison(){
+//    TreeSet tree1 = new TreeSet();
+//    TreeSet tree2 = new TreeSet();
+//
+//    tree1.add(1);
+//    tree1.add(2);
+//    tree1.add(3);
+//    tree1.add(4);
+//
+//    tree2.add(1);
+//    tree2.add(4);
+//    tree2.add(9);
+//    tree2.add(16);
+//
+//    TreeIntersection treeIntersection = new TreeIntersection();
+//    System.out.println(treeIntersection.treeComparing(tree1,tree2).toString());
+//  }
 }
